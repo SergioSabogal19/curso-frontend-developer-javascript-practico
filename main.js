66120da6501ba21f84ca94192c394de2e7f2eaa2
Sergio Sabogal <sdsabogalq@correo.udistrital.edu.co>
2023-08-26 21:20:57 -0500

    Vinculación de menu para mobile

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 const navEmail = document.getElementsByClassName('navbar-email');
 const desktopMenu= document.getElementsByClassName('desktop-menu');
+const iconMobileMenu = document.getElementsByClassName('menu');
+const mobileMenu = document.getElementsByClassName('mobile-menu');
+
 
 navEmail[0].addEventListener('click', toggleMenu);
+iconMobileMenu[0].addEventListener('click', toggleMobileMenu);
 
 function toggleMenu() {
 
     desktopMenu[0].classList.toggle('inactive');
 
 }
+
+function toggleMobileMenu() {
+
+    mobileMenu[0].classList.toggle('inactive');
+
+}
+
 console.log('funcionando');
